c357bcc6d64f2450f46ee2ec549339aa87fa3a77
aridgway120 <aridgway@ualberta.ca>
2019-02-15 23:10:09 +0000

    all the stuff

---
+++ react-weather-app/build/precache-manifest.f85503ac4d343245a9aa6f532b1487c9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "673c9674cc649f288158",
+    "url": "/static/js/main.673c9674.chunk.js"
+  },
+  {
+    "revision": "947d507a2eb31f2f56b4",
+    "url": "/static/js/1.947d507a.chunk.js"
+  },
+  {
+    "revision": "673c9674cc649f288158",
+    "url": "/static/css/main.c247d89f.chunk.css"
+  },
+  {
+    "revision": "ad95fbe93f8ed7732df1fb3ded26a3e0",
+    "url": "/index.html"
+  }
+];
